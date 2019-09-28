081a16cb6b50dc937c39253e6f3f6bbb6c27495a
Reeyou <ryu993830137@163.com>
2019-09-28 22:42:57 +0800

    管理端登录接口更新

--- routes/route/users.js
+++ routes/route/users.js
@@ -2,29 +2,35 @@ const router = require('koa-router')()
 
 const User = require('../../models/user')
 
-router.prefix('/users')
-
+router.prefix('/admin')
 router.post('/login', async(ctx, next) => {
   const params = ctx.request.body
-  console.log(params)
-  const user = new User({
-    username: params.username,
-    password: params.password
-  })
+
+  const username = params.username
+  const password = params.password
   let code, msg
   try {
-    await user.save()
-    code = 200
-    msg = '添加成功'
+    await User.find({username},function(err, data) {
+      if(data.length === 0) {
+        code = -1
+        msg = '用户不存在'
+      } else if (data[0].password === password) {
+        code = 200
+        msg = '登录成功'
+      } else if (data[0].password !== password) {
+        code = 400
+        msg = '密码错误'
+      }
+    })
   } catch (e) {
     code = -1
-    msg = '添加失败'
+    msg = '登录失败'
   }
 
   ctx.response.body = {
     code: code,
     msg: msg,
-    data: ctx.request.body
+    // data: ctx.request.body
   }
 })
 
